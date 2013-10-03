cd42ed53931688c959094a79b94dadb05c4f31a7
Jie Feng <fj333xx@gmail.com>
2013-10-03 15:36:18 -0400

    trained sh on train_1

--- Hashing/hashall.m
+++ Hashing/hashall.m
@@ -10,11 +10,32 @@ feat_file = 'F:\Datasets\Recognition\CIFAR-10\cifar-10-binary\train_1_data.txt';
 
 [feats labels] = loadfeatfile(feat_file);
 
+% params
+nbits = 32;
+
 % compute sh
-SHparams.nbits = 32;
+SHparams.nbits = nbits;
 SHparams = trainSH(feats, SHparams);
 [sh_codes, U] = compressSH(feats, SHparams);
 % convert sh code to unified code rep: array of int 0/1
+% concatenate all char
+sh_code_str = [];
+for r=1:size(sh_codes,1)
+    cur_row = [];
+    for c=1:size(sh_codes,2)
+        cur_code = dec2bin(sh_codes(r,c), 8);
+        cur_row = [cur_row cur_code];
+    end
+    sh_code_str = [sh_code_str; cur_row];
+end
+sh_code_num = int8(sh_code_str-'0');
+
+% hash code data file format:
+% #samp #feat_dim #cls_num
+% label code_Str
+sh_data = double([labels sh_code_num]);
+save('cifar10_train1.code', 'sh_data', '-ascii');
 
 
-% compute weights
+
+% learn weights
